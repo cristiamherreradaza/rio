186a80db89e72d83f705b8eebfe12b4b6e8951e5
Cristiam Herrera <cristiamherrera@gmail.com>
2021-03-06 11:12:15 -0400

    se crearon las migraciones para asistencias y pagos

---
+++ database/migrations/2021_03_06_145643_create_pagos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedDecimal('monto', 15, 2);
+            $table->string('mes', 15)->nullable();
+            $table->string('gestion', 4)->nullable();
+            $table->datetime('fecha_pago')->nullable();
+            $table->string('tipo', 30)->nullable();
+            $table->string('estado', 30)->nullable();
+            $table->datetime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pagos');
+    }
+}
