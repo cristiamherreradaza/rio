2fd9ded97de8f8caf659b38df16a5777b467ac7e
Cristiam Herrera <cristiamherrera@gmail.com>
2021-03-06 10:56:18 -0400

    Se adiciono la migracion de eventos

---
+++ database/migrations/2021_03_06_141825_create_eventos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('nombre', 255)->nullable();
+            $table->text('descripcion')->nullable();
+            $table->string('imagen', 150)->nullable();
+            $table->datetime('fecha_inicio')->nullable();
+            $table->datetime('fecha_fin')->nullable();
+            $table->string('tipo', 30)->nullable();
+            $table->string('estado', 30)->nullable();
+            $table->datetime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+}
