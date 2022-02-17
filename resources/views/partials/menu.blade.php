b039f4f06a9c0c1ab3f2fee4c49487f8582ce4d2
Cristiam Herrera <cristiamherrera@gmail.com>
2022-02-17 11:27:52 -0400

    Se cambio los menus

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -39,17 +39,13 @@
 						</svg>
 						<!--end::Svg Icon-->
 					</span>
-					<span class="menu-text">Usuarios</span>
+					<span class="menu-text">Socios</span>
 					<i class="menu-arrow"></i>
 				</a>
 				<div class="menu-submenu">
 					<i class="menu-arrow"></i>
 					<ul class="menu-subnav">
-						<li class="menu-item menu-item-parent" aria-haspopup="true">
-							<span class="menu-link">
-								<span class="menu-text">Usuarios</span>
-							</span>
-						</li>
+						
 						<li class="menu-item" aria-haspopup="true">
 							<a href="{{ url('User/listado') }}" class="menu-link">
 								<i class="menu-bullet menu-bullet-dot">
@@ -104,6 +100,104 @@
 					</ul>
 				</div>
 			</li>
+
+			<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+				<a href="javascript:;" class="menu-link menu-toggle">
+					<span class="svg-icon menu-icon">
+						<!--begin::Svg Icon | path:assets/media/svg/icons/Home/Book-open.svg-->
+						<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
+							height="24px" viewBox="0 0 24 24" version="1.1">
+							<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+								<rect x="0" y="0" width="24" height="24" />
+								<path
+									d="M6,2 L18,2 C19.6568542,2 21,3.34314575 21,5 L21,19 C21,20.6568542 19.6568542,22 18,22 L6,22 C4.34314575,22 3,20.6568542 3,19 L3,5 C3,3.34314575 4.34314575,2 6,2 Z M12,11 C13.1045695,11 14,10.1045695 14,9 C14,7.8954305 13.1045695,7 12,7 C10.8954305,7 10,7.8954305 10,9 C10,10.1045695 10.8954305,11 12,11 Z M7.00036205,16.4995035 C6.98863236,16.6619875 7.26484009,17 7.4041679,17 C11.463736,17 14.5228466,17 16.5815,17 C16.9988413,17 17.0053266,16.6221713 16.9988413,16.5 C16.8360465,13.4332455 14.6506758,12 11.9907452,12 C9.36772908,12 7.21569918,13.5165724 7.00036205,16.4995035 Z"
+									fill="#000000" />
+							</g>
+						</svg>
+						<!--end::Svg Icon-->
+					</span>
+					<span class="menu-text">Reportes</span>
+					<i class="menu-arrow"></i>
+				</a>
+				<div class="menu-submenu">
+					<i class="menu-arrow"></i>
+					<ul class="menu-subnav">
+			
+						<li class="menu-item" aria-haspopup="true">
+							<a href="{{ url('User/listado') }}" class="menu-link">
+								<i class="menu-bullet menu-bullet-dot">
+									<span></span>
+								</i>
+								<span class="menu-text">Pagos</span>
+							</a>
+						</li>
+			
+						<li class="menu-item" aria-haspopup="true">
+							<a href="{{ url('User/listado') }}" class="menu-link">
+								<i class="menu-bullet menu-bullet-dot">
+									<span></span>
+								</i>
+								<span class="menu-text">Eventos</span>
+							</a>
+						</li>
+			
+					</ul>
+				</div>
+			</li>
+
+			<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+				<a href="javascript:;" class="menu-link menu-toggle">
+					<span class="svg-icon menu-icon">
+						<!--begin::Svg Icon | path:assets/media/svg/icons/Home/Book-open.svg-->
+						<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
+							height="24px" viewBox="0 0 24 24" version="1.1">
+							<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+								<rect x="0" y="0" width="24" height="24" />
+								<path
+									d="M6,2 L18,2 C19.6568542,2 21,3.34314575 21,5 L21,19 C21,20.6568542 19.6568542,22 18,22 L6,22 C4.34314575,22 3,20.6568542 3,19 L3,5 C3,3.34314575 4.34314575,2 6,2 Z M12,11 C13.1045695,11 14,10.1045695 14,9 C14,7.8954305 13.1045695,7 12,7 C10.8954305,7 10,7.8954305 10,9 C10,10.1045695 10.8954305,11 12,11 Z M7.00036205,16.4995035 C6.98863236,16.6619875 7.26484009,17 7.4041679,17 C11.463736,17 14.5228466,17 16.5815,17 C16.9988413,17 17.0053266,16.6221713 16.9988413,16.5 C16.8360465,13.4332455 14.6506758,12 11.9907452,12 C9.36772908,12 7.21569918,13.5165724 7.00036205,16.4995035 Z"
+									fill="#000000" />
+							</g>
+						</svg>
+						<!--end::Svg Icon-->
+					</span>
+					<span class="menu-text">Configuraciones</span>
+					<i class="menu-arrow"></i>
+				</a>
+				<div class="menu-submenu">
+					<i class="menu-arrow"></i>
+					<ul class="menu-subnav">
+						
+						<li class="menu-item" aria-haspopup="true">
+							<a href="{{ url('User/listado') }}" class="menu-link">
+								<i class="menu-bullet menu-bullet-dot">
+									<span></span>
+								</i>
+								<span class="menu-text">Usuarios</span>
+							</a>
+						</li>
+
+						<li class="menu-item" aria-haspopup="true">
+							<a href="{{ url('User/listado') }}" class="menu-link">
+								<i class="menu-bullet menu-bullet-dot">
+									<span></span>
+								</i>
+								<span class="menu-text">Categorias</span>
+							</a>
+						</li>
+
+						<li class="menu-item" aria-haspopup="true">
+							<a href="{{ url('User/listado') }}" class="menu-link">
+								<i class="menu-bullet menu-bullet-dot">
+									<span></span>
+								</i>
+								<span class="menu-text">Parametros</span>
+							</a>
+						</li>
+			
+					</ul>
+				</div>
+			</li>
+
 		</ul>
 		<!--end::Menu Nav-->
 	</div>
