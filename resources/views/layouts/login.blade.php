6b15589ddcec28238ce56755750a8ab6df04d06d
Cristiam Herrera <cristiamherrera@gmail.com>
2022-03-02 12:23:47 -0400

    Se cambio el formulario de registro de doctores

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -77,14 +77,14 @@ License: You must have a valid license purchased only from themeforest(the above
 						<!--begin::Signup-->
 						<div class="login-form login-signup">
 							<div class="text-center mb-10 mb-lg-20">
-								<h3 class="">Sign Up</h3>
-								<p class="text-muted font-weight-bold">Enter your details to create your account</p>
+								<h3 class="">FORMULARIO DE REGISTRO</h3>
+								{{-- <p class="text-muted font-weight-bold">Enter your details to create your account</p> --}}
 							</div>
 							<!--begin::Form-->
 							<form class="form" novalidate="novalidate" id="kt_login_signup_form">
 								<div class="form-group py-3 m-0">
 									<input class="form-control h-auto border-0 px-0 placeholder-dark-75" type="text"
-										placeholder="Fullname" name="fullname" autocomplete="off" />
+										placeholder="Nombre Completo" name="nombre" autocomplete="off" />
 								</div>
 								<div class="form-group py-3 border-top m-0">
 									<input class="form-control h-auto border-0 px-0 placeholder-dark-75" type="password"
@@ -94,23 +94,12 @@ License: You must have a valid license purchased only from themeforest(the above
 									<input class="form-control h-auto border-0 px-0 placeholder-dark-75" type="password"
 										placeholder="Password" name="password" autocomplete="off" />
 								</div>
-								<div class="form-group py-3 border-top m-0">
-									<input class="form-control h-auto border-0 px-0 placeholder-dark-75" type="password"
-										placeholder="Confirm password" name="cpassword" autocomplete="off" />
-								</div>
-								<div class="form-group mt-5">
-									<div class="checkbox-inline">
-										<label class="checkbox checkbox-outline">
-											<input type="checkbox" name="agree" />
-											<span></span>I Agree the
-											<a href="#" class="ml-1">terms and conditions</a>.</label>
-									</div>
-								</div>
+								
 								<div class="form-group d-flex flex-wrap flex-center">
 									<button id="kt_login_signup_submit"
-										class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-2">Submit</button>
+										class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-2">Registro</button>
 									<button id="kt_login_signup_cancel"
-										class="btn btn-outline-primary font-weight-bold px-9 py-4 my-3 mx-2">Cancel</button>
+										class="btn btn-outline-primary font-weight-bold px-9 py-4 my-3 mx-2">Cancelar</button>
 								</div>
 							</form>
 							<!--end::Form-->
